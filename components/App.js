4bfd29ad5fe3810ebacd68906b962435bfd45811
Mila Bozhevskaya <mi.bozhev@gmail.com>
2023-10-14 11:19:29 +0300

    feat: add textarea

---
+++ components/App.js
@@ -0,0 +1,6 @@
+import { Element } from './Element.js';
+import { Textarea  } from './textarea/textarea.js';
+
+export const App = (parent, content) => {
+  const textarea = new Textarea(parent);
+};

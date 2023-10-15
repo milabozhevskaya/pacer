88db4206c6fdd9fbd5d41865b82688d1ad5e666e
Mila Bozhevskaya <mi.bozhev@gmail.com>
2023-10-15 11:40:58 +0300

    feat: sync ta value with ls

--- components/App.js
+++ components/App.js
@@ -1,6 +1,13 @@
-import { Element } from './Element.js';
 import { Textarea  } from './textarea/textarea.js';
+import { Store } from './Store.js';
+import { Controller } from './Controller.js';
 
 export const App = (parent, content) => {
-  const textarea = new Textarea(parent);
+  const store = new Store();
+  const controller = new Controller(store);
+
+  const textarea = new Textarea(parent, controller.changeTextareaText);
+  store.onChangeTextareaText.add(textareaText => textarea.update(textareaText));
+  
+  controller.init(content => textarea.update(content));
 };

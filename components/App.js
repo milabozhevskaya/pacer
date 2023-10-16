93fe35210ffe91f6fb81c6637922b251cfed4f3c
Mila Bozhevskaya <mi.bozhev@gmail.com>
2023-10-16 10:46:22 +0300

    feat: add main title (P-003-F)

--- components/App.js
+++ components/App.js
@@ -1,3 +1,4 @@
+import { Element } from "./Element.js";
 import { Textarea  } from './textarea/textarea.js';
 import { Store } from './Store.js';
 import { Controller } from './Controller.js';
@@ -5,6 +6,8 @@ import { Controller } from './Controller.js';
 export const App = (parent, content) => {
   const store = new Store();
   const controller = new Controller(store);
+  
+  const title = new Element({ parent, tagName: 'h1', content: content.title });
 
   const textarea = new Textarea(parent, controller.changeTextareaText);
   store.onChangeTextareaText.add(textareaText => textarea.update(textareaText));

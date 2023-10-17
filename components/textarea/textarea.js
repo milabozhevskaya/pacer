e051bcf55245f1a6c0a3cec40180c1b0578c7893
Mila Bozhevskaya <mi.bozhev@gmail.com>
2023-10-17 10:42:34 +0300

    fix: bug with merge resolve

--- components/textarea/textarea.js
+++ components/textarea/textarea.js
@@ -1,13 +1,4 @@
 import { Element } from "../Element.js";
-<<<<<<< HEAD
-import { styles } from './style.js';
-
-class Textarea extends Element {
-  constructor(parent) {
-    super({ parent, tagName: "textarea", styles });
-    this.node.rows = '6';
-  }
-=======
 import { styles } from "./style.js";
 
 class Textarea extends Element {
@@ -18,7 +9,6 @@ class Textarea extends Element {
   }
 
   update = (text) => (this.node.value = text);
->>>>>>> 88db4206c6fdd9fbd5d41865b82688d1ad5e666e
 }
 
 export { Textarea };

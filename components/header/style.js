624dc0d2df43bee55ceb3bee66e1dc2dd0a94ea3
Mila Bozhevskaya <mi.bozhev@gmail.com>
2023-10-18 16:52:00 +0300

    feat: add changing of self-belief-points

--- components/header/style.js
+++ components/header/style.js
@@ -1,9 +1,11 @@
 export const styles = {
   width: '100%',
-  padding: '16px',
+  paddingTop: '16px',
+  paddingBottom: '16px',
   minHeight: '52px',
   container: {
     display: 'flex',
     justifyContent: 'space-between',
+    alignItems: 'center',
   }
 }
--- components/self-belief-points/SelfBeliefPoints.js
+++ components/self-belief-points/SelfBeliefPoints.js
@@ -9,33 +9,67 @@ class SelfBeliefPoints extends Element {
       className: `${className}__self-belief-points self-belief-points`,
       styles,
     });
-    this.container = new Element({
-      parent: this.node,
-      className: "self-belief-points__container container",
-    });
     this.label = new Element({
-      parent: this.container.node,
+      parent: this.node,
       tagName: "label",
       className: "self-belief-points__label",
+      styles: styles.label,
+    });
+    this.span = new Element({
+      parent: this.label.node,
+      tagName: "span",
+      className: "self-belief-points__span",
+      styles: styles.span,
+      content: "0",
     });
     this.input = new Element({
       parent: this.label.node,
       tagName: "input",
       className: "self-belief-points__input",
+      styles: { ...styles.input },
     });
+    this.node.onfocus = () => {};
+    this.node.onblur = () => {};
+    this.input.node.onfocus = () => {
+      Object.assign(this.input.node.style, styles.input.onfocus);
+      Object.assign(this.node.style, styles.onfocus);
+    };
+    this.input.node.onblur = () => {
+      Object.assign(this.input.node.style, styles.input);
+      Object.assign(this.node.style, styles);
+    };
     this.input.node.type = "number";
     this.input.node.min = "0";
     this.input.node.step = "1";
+    this.input.node.value = "0";
     this.input.node.onkeydown = (event) => {
-      if ( ['+', '-', 'e', ',', '.'].includes(event.key)) event.preventDefault();
-    //   if (event.key === "Enter") {
-    //     controller.updateSelfBeliefPoints(this.input.node.value);
-      
-    // }
+      if (["+", "-", "e", ",", "."].includes(event.key)) {
+        this.flag = true;
+        event.preventDefault();
+        return;
+      }
+      if (event.key === "Enter" || event.key === "Escape") {
+        this.input.node.blur();
+        return;
+      }
+    };
+    this.input.node.oninput = (event) => {
+      if (!this.flag) {
+        this.update(this.input.node.value);
+      }
+    };
+    this.input.node.onchange = () => {
+      this.input.node.value = parseInt(this.input.node.value);
+      this.update(this.input.node.value);
+    };
+    this.input.node.onkeyup = (event) => {
+      this.flag = false;
     };
   }
 
-  update = (points) => (this.input.node.value = points);
+  update = (points) => {
+    this.span.node.innerText = points;
+  };
 }
 
 export { SelfBeliefPoints };
